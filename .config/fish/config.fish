0f20448a8ca06a508b8c882a68bb8b5287e164d4
James Fischer <contact@jamesfischer.dev>
2024-12-31 19:40:49 -0800

    Make IRB autocomplete more humble
    Now it waits for tab to offer suggestions.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ set -x PATH /opt/local/bin /opt/local/sbin $PATH
 source /opt/local/share/asdf/asdf.fish
 set -gx EDITOR nvim
 set -gx VISUAL nvim
+set -gx IRB_USE_AUTOCOMPLETE false
+set -gx IRB_COMPLETOR type
