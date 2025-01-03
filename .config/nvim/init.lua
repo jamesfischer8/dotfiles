23897f6e8965fa88e4b9a0501dfa78665afb8f90
James Fischer <contact@jamesfischer.dev>
2025-01-02 19:57:16 -0800

    Add syntax highlighting (catppuccin) for Neovim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,26 @@
+-- Enable built-in filetype detection and syntax highlighting
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')
+
+-- Basic editor settings
+vim.opt.number = true          -- Show line numbers
+vim.opt.syntax = 'on'          -- Enable syntax highlighting
+vim.opt.termguicolors = true   -- Enable true color support
+
+-- Set up Treesitter for syntax highlighting
+require('nvim-treesitter.configs').setup({
+  ensure_installed = { "lua", "vim", "javascript", "ruby", "python", "html", "css" },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false, -- Disable old syntax highlighting
+  },
+  indent = {
+    enable = true,
+  },
+})
+
+-- Theme setup
+require("catppuccin").setup({
+    flavour = "macchiato"
+})
+vim.cmd([[colorscheme catppuccin]])
