a1b0049c9f9aeb82fa08e91a06d2194aae6191ed
James Fischer <contact@jamesfischer.dev>
2025-01-04 14:08:58 -0800

    Set Neovim to soft wrap on spaces and punctuation
    The default value for breakat is ` ^I!@*-+;:,./?`.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,9 @@ vim.opt.shiftwidth = 2      -- Number of spaces for auto-indent
 vim.opt.autoindent = true   -- Copy indent from current line when starting a new line
 vim.opt.smartindent = true  -- Smart autoindenting when starting a new line
 
+-- Wrap on whole words
+vim.opt.linebreak = true
+
 -- Enable built-in filetype detection and syntax highlighting
 vim.cmd('syntax enable')
 vim.cmd('filetype plugin indent on')
